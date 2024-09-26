b95719d2f815345ece91d3b6be4f749183f70938
fab <fabrix336@gmail.com>
2024-09-25 22:15:26 -0500

    Registrar e Iniciar Sesion

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alquileres
+Lo que hice es usar app e ir probando los componentes
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+import { RegistrarComponent } from './registrar/registrar.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { MainComponent } from './main/main.component';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistrarComponent, IniciarSesionComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'alquiangu';
+  title = 'alquileres';
 }
